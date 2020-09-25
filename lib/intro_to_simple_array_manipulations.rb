6e1eb9708dd30400f42dce873aea67bf904b2041
Naga Bhargavi Marudhuri <bhargavimarudhuri@gmail.com>
2020-09-25 20:38:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,8 @@ def using_concat(array1, array2)
     array1.concat(array2)
 end   
 
-def using_insert(array, element = 3)
+def using_insert(array, element)
+    element = 3
     array.insert(element)
 end  
 
